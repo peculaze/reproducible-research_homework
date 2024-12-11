cfc82a47dc4ef576e8260e248c16397920cd9e95
peculaze <max.sharman@wadham.ox.ac.uk>
2024-12-11 17:11:31 +0000

    5d 5e

--- virus_analysis.R
+++ virus_analysis.R
@@ -10,15 +10,24 @@ virus_data_log <- virus_data
 virus_data_log$log.genome.length = log(virus_data$Genome.length..kb.)
 virus_data_log$log.virion.volume = log(virus_data$Virion.volume..nm.nm.nm.)
 
+# Code for 5.4): 
 ggplot(virus_data_log, aes(x=log.genome.length, y=log.virion.volume)) + 
-  geom_point() 
-ggplot(virus_data_log, aes(x=log.genome.length, y=log.virion.volume)) + 
-  geom_point() 
+  geom_point() + 
+  theme_bw() +
+  geom_smooth(method = "lm") + 
+  labs(x = "log [Genome length (kb)]", y = "log [Virion volume (nm3)]") + 
+  theme(axis.title = element_text(face = 2), size = 10)
+
+# Code for 5.5): 
+
+1181.807*300^(1.5152)
 
 virus_log_lm = lm(log.virion.volume ~ log.genome.length, data = virus_data_log)
 
 summary(virus_log_lm)
 plot(virus_log_lm)
+
+exp(7.0748)
 
 cor.test(virus_data_log$log.genome.length, virus_data_log$log.virion.volume, method = "pearson")
 cor.test(virus_data$Genome.length..kb., virus_data$Virion.volume..nm.nm.nm., method = "pearson")
