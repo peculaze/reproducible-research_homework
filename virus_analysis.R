6bd1dcf11cea2d9fb31fe74b77165539315be584
peculaze <max.sharman@wadham.ox.ac.uk>
2024-12-13 06:39:51 +0000

    add renv, update code markings

--- virus_analysis.R
+++ virus_analysis.R
@@ -3,9 +3,11 @@ virus_data = read.csv("question-5-data/Cui_etal2014.csv")
 
 summary(virus_data)
 
+# Plot untransformed data
 ggplot(virus_data, aes(x=Genome.length..kb., y=Virion.volume..nm.nm.nm.)) + 
   geom_point() 
 
+# Transform data
 virus_data_log <- virus_data
 virus_data_log$log.genome.length = log(virus_data$Genome.length..kb.)
 virus_data_log$log.virion.volume = log(virus_data$Virion.volume..nm.nm.nm.)
@@ -29,7 +31,6 @@ plot(virus_log_lm)
 
 exp(7.0748)
 
-cor.test(virus_data_log$log.genome.length, virus_data_log$log.virion.volume, method = "pearson")
-cor.test(virus_data$Genome.length..kb., virus_data$Virion.volume..nm.nm.nm., method = "pearson")
-
-
+# Save package versions
+#renv::init()
+renv::snapshot()
